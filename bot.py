591ae3ac9bc2674c1b2e569a67f10f2e56efa402
Timur Shagaliev <shagaliev.timur.s@yandex.ru>
2022-04-16 16:00:39 +0300

    vg

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import telebot
+from telebot import types # для указание типов
+import config
 
 # Создаем экземпляр бота
 
@@ -6,27 +8,36 @@ bot = telebot.TeleBot('5272235435:AAHpP-PpRGuy1CAnAnug3-zxLW7lZcW2Wnw')
 
 # Функция, обрабатывающая команду /start
 
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands=["start","help"])
+def welcome(message):
+    hello = bot.send_message(message.chat.id,"Сезне сәламлим!")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    mrk = types.KeyboardButton("Халык ашлары")
+    mrk1 = types.KeyboardButton("Суыткыч")
+    mrk2 = types.KeyboardButton("Яраткан ашлар")
+    markup.add(mrk,mrk1,mrk2)
+    bot.send_message(message.chat.id, "Сайлагыз менюга, бу сезгә кызык", reply_markup=markup)
 
-def start(m, res=False):
 
-    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+@bot.message_handler(content_types=['text'])
+def start_text(message):
+    if message.text == "Каталог":
+        markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        mar = types.KeyboardButton("Одежда")
+        mar2 = types.KeyboardButton("Назад в меню")
+        markup1.add(mar,mar2)
+        bot.send_message(message.chat.id,"Выберите, что вам нужно, если хотите вернуться в меню просто нажмите кнопку 'Назад в меню' ",reply_markup=markup1)
+    elif message.text == 'Корзина':
+        markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        mar = types.KeyboardButton("Одежда")
+        mar2 = types.KeyboardButton("Назад в меню")
+        markup1.add(mar, mar2)
+        bot.send_message(message.chat.id,
+                         "Выберите, что вам нужно, если хотите вернуться в меню просто нажмите кнопку 'Назад в меню' ",
+                         reply_markup=markup1)
+    elif message.text == "Назад в меню":
+        welcome(message)
 
-# Получение сообщений от юзера
-
-@bot.message_handler(content_types=["text"])
-
-def handle_text(message):
-    if 'дела' in message.text.lower():
-        bot.send_message(message.chat.id, 'Хорошо)')
-    elif 'здрав' in message.text.lower():
-        bot.send_message(message.chat.id, 'Здравствуйте!')
-    elif 'прив' in message.text.lower():
-        bot.send_message(message.chat.id, 'Привет!')
-    elif 'адрес' in message.text.lower():
-        bot.send_message(message.chat.id, 'г. Казань, ул. Деревни Универсиады,  д. 32')
-    elif 'телефон' in message.text.lower():
-        bot.send_message(message.chat.id, '77777777777')
 
 
 
--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 s = input()
-a = s.split('.')
-for i in  range(len(a)):
-    if i == 0:
-        print(str(i+1)+'.'+a[i]+'.'+' ', end='')
-    elif i == len(a)-2:
-        print(str(i + 1) + '.' + a[i] + '.' + ' ')
-        break
-    else:
-        print(str(i + 1) + '.'  + a[i] + '.' + ' ', end=' ')
+a = s.split('. ')
+a[-1] = a[-1][:-1]
+s1 = '\\n'
+for i in range(len(a)):
+        print(f'{str(i+1)}.{a[i]}.{s1}', end='')
